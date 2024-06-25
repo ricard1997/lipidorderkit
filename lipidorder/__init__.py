7c8eb1b56190f2331406217ae9447f7abbfff786
Ricardo Ramirez <ricar.d1997@hotmail.com>
2024-06-24 23:49:34 -0400

    Initial creation with MDAnalysis Cookiecutter 0.1

---
+++ lipidorder/__init__.py
@@ -0,0 +1,9 @@
+"""
+lipidorderkit
+Program designed to compute order parameters for all atom MD simulations
+"""
+
+# Add imports here
+from importlib.metadata import version
+
+__version__ = version("lipidorderkit")
