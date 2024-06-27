6fa9af4078ebb906656e5ce375e69bd1eb69e6c1
Ricardo Ramirez <ricar.d1997@hotmail.com>
2024-06-27 18:16:57 -0400

    changes related to documentation

--- lipidorder/__init__.py
+++ lipidorder/__init__.py
@@ -1,9 +1,7 @@
-"""
-lipidorderkit
-Program designed to compute order parameters for all atom MD simulations
-"""
 
 # Add imports here
 from importlib.metadata import version
+
+
 
 __version__ = version("lipidorderkit")
