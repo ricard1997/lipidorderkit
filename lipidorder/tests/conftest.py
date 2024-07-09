aba4f7e57eae7caf40824f91bec81abbf11e6e78
Ricardo Ramirez <ricar.d1997@hotmail.com>
2024-07-09 11:59:38 -0400

    delete default fixture

--- lipidorder/tests/conftest.py
+++ lipidorder/tests/conftest.py
@@ -12,9 +12,9 @@ import pytest
 from lipidorder.data.files import MDANALYSIS_LOGO
 
 
-@pytest.fixture
-def mdanalysis_logo_text() -> str:
-    """Example fixture demonstrating how data files can be accessed"""
-    with open(MDANALYSIS_LOGO, "r", encoding="utf8") as f:
-        logo_text = f.read()
-    return logo_text
+#@pytest.fixture
+#def mdanalysis_logo_text() -> str:
+#    """Example fixture demonstrating how data files can be accessed"""
+#    with open(MDANALYSIS_LOGO, "r", encoding="utf8") as f:
+#        logo_text = f.read()
+#    return logo_text
